5f70f6bf448c2928a7e2125bb27a70698c50ac45
ChuckMo1019 <69222423+ChuckMo1019@users.noreply.github.com>
2020-11-04 19:31:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function inputOne(){
+  alert("You clicked on " + '"small"')
+}
+function inputTwo(){
+  alert("You clicked on " + '"medium"')
+}
+function inputThree(){
+  alert("You clicked on " + '"large"')
+}
